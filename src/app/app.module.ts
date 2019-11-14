1bc5dcddb6b8d60c134e7afd8f9d760609ea1e6b
Jesus Moreno <jesus.moreno@janaq.com>
2019-11-13 22:02:31 -0500

    Filter service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { C404Component } from './components/c404/c404.component';
+import { FormServiceComponent } from './components/form-service/form-service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    C404Component,
+    FormServiceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Servicio } from '../../models/Servicio';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public titulo:string;
+  public servicios:Array<Servicio>;
+  public filServicios:Array<Servicio>;
+  public mostrar:boolean;
+  public tipos:Array<any>;
+  constructor() {
+    this.titulo="Servicios";
+    this.servicios=[
+      new Servicio(1,'Auto','abc',1),
+      new Servicio(2,'Salud','abcd',2),
+      new Servicio(3,'Salud','axy',2),
+      new Servicio(4,'Hogar','axy',3),
+      new Servicio(5,'Hogar','axy',3)
+     ];
+     this.filServicios=[];
+     this.mostrar=true;
+     this.tipos=[
+       {id:0,nombre:"Todos"},
+       {id:1,nombre:"Autos"},
+       {id:2,nombre:"Salud"},
+       {id:3,nombre:"Hogar"}
+     ];
+  }
+
+  ngOnInit() {//inicializa el componente 
+   //console.log(this.servicios);
+   this.getServices(0);//todos
+  }
+  
+  getServices(tipo:number){
+    this.filServicios=[];
+    if(tipo){//1,2,3
+    this.servicios.forEach((item,index)=>{
+      if(tipo==item.tipo){
+        this.filServicios.push(item);
+      }
+    });
+    } else {//tipo=0 (todos)
+      this.filServicios=this.servicios;
+      console.log(this.filServicios);
+    }
+  }
+
+}
